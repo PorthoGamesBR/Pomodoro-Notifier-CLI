916bb33ed79c466f57b67b1ee08b70d844998305
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-01-28 10:57:36 -0300

    Created functions for notification

--- pomotimer.py
+++ pomotimer.py
@@ -9,17 +9,24 @@ parser.add_argument('-r', '--rest', help='Rest time', type=int, required=False)
 
 args = parser.parse_args()
 
+toaster = ToastNotifier()
+def end_phase_notify(phase, time):
+    toaster.show_toast("Pomotimer", f"{phase} time has ended. Take {time} minutes of rest!", duration=10)
+    
+def start_pomo(time_work, time_rest):
+    print(f"Pomodoro timer started with {time_work} minutes of work time and {time_rest} minutes of rest time.")
+    print("Good work and remember to concentrate!")
+    print()
+    end_phase_notify("Work",time_rest)
+    end_phase_notify("Rest",time_work)
+    
 def main():
     # in minutes
     time_work = default_times['w'] if not args.work else args.work
     time_rest = default_times['r'] if not args.rest else args.rest
     
-    print(f"Pomodoro timer started with {time_work} minutes of work time and {time_rest} minutes of rest time.")
-    print("Good work and remember to concentrate!")
-    print()
+    start_pomo(time_work, time_rest)
     
-    toaster = ToastNotifier()
-    toaster.show_toast("Title", "Hello, World!", duration=2)
     
 if __name__ == "__main__":
     main()
