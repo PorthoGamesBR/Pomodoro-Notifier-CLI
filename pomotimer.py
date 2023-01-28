52949ff4c259ef473f762225de4e482622f0e1d2
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-01-28 11:08:51 -0300

    Timers added to start_pomo

--- pomotimer.py
+++ pomotimer.py
@@ -1,7 +1,10 @@
 from win10toast import ToastNotifier
 import argparse
+import time
+import datetime
 
 default_times = {'w':30,'r':15}
+pomo_running = False
 
 parser = argparse.ArgumentParser(description='This is a pomodoro timer')
 parser.add_argument('-w', '--work', help='Work time', type=int, required=False)
@@ -17,8 +20,23 @@ def start_pomo(time_work, time_rest):
     print(f"Pomodoro timer started with {time_work} minutes of work time and {time_rest} minutes of rest time.")
     print("Good work and remember to concentrate!")
     print()
-    end_phase_notify("Work",time_rest)
-    end_phase_notify("Rest",time_work)
+    
+    tw = datetime.timedelta(minutes=time_work)
+    tr = datetime.timedelta(minutes=time_rest)
+    
+    pomo_running = True
+    pomo_ammount = 3
+    
+    while pomo_running:
+        pomo_ammount-=1
+        time.sleep(tw.total_seconds())
+        end_phase_notify("Work",time_rest)
+        time.sleep(tr.total_seconds())
+        end_phase_notify("Rest",time_work)
+        if pomo_ammount == 0:
+            pomo_running = False
+            
+    print("Pomotimer ended. Thanks for your time and hope to see you soon!")
     
 def main():
     # in minutes
