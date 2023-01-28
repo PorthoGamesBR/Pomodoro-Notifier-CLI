c5bd1a81301d27549a609c18ad62ed0e0702bf57
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-01-28 10:44:52 -0300

    Added first message

--- pomotimer.py
+++ pomotimer.py
@@ -1,6 +1,13 @@
 from win10toast import ToastNotifier
 
 def main():
+    # in minutes
+    time_work = 0 
+    time_rest = 0
+    
+    print(f"Pomodoro timer started with {time_work} minutes of work time and {time_rest} minutes of rest time.")
+    print("Good work and remember to concentrate!")
+    
     toaster = ToastNotifier()
     toaster.show_toast("Title", "Hello, World!", duration=5)
     
